9d3f9977a1c687ca2b3672bade8014f9032ef02c
MarcM <marina.ved80gmail.com>
2024-10-04 16:32:48 -0600

    add Footer with starter content

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import { Link, useLocation } from 'react-router-dom';
+
+function NavTabs() {
+  const currentPage = useLocation().pathname;
+
+  return (
+    <div>
+        FOOTER is coming soon
+    </div>
+  );
+}
+
+export default NavTabs;
