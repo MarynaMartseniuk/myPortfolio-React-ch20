949d044ef05855d9e1da9c3d63acfb46acf8d512
MarcM <marina.ved80gmail.com>
2024-10-04 12:26:35 -0600

    add App.jsx and NavTabs.jsx-component

---
+++ src/components/NavTabs.jsx
@@ -0,0 +1,32 @@
+import { Link, useLocation } from 'react-router-dom';
+
+function NavTabs() {
+  const currentPage = useLocation().pathname;
+
+  return (
+    <ul>
+      <li>
+        <Link to="/">
+          About
+        </Link>
+      </li>
+      <li>
+        <Link to="/Portfolio">
+          Portfolio
+        </Link>
+      </li>
+      <li>
+        <Link to="/Contact">
+          Contact
+        </Link>
+      </li>
+      <li>
+        <Link to="/Resume">
+          Resume
+        </Link>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
