11ade572699e02e36f2c8a07050acd17120f1627
MarcM <marina.ved80gmail.com>
2024-10-04 16:25:42 -0600

    add pages and some starter content

---
+++ src/pages/About.jsx
@@ -0,0 +1,25 @@
+import myImage from '../utils/images/m2.jpg'
+
+export default function About() {
+    return (
+      <div>
+        <h1>About Me</h1>
+        <img src={myImage}/>
+        <div >
+          <p>
+            My name is Maryna.
+            I have a family (husband and son).
+            I have 3 parrots.
+          </p>
+          <p>
+            I am currenly working for a medical company. 
+            My possition is a Process Control Operator.
+          </p>
+          <p>
+            I have a Masters Degree in Mechanial Engineering.
+            I have been taken Coding Bootcamp at UofU.
+          </p>
+        </div>
+      </div>
+    );
+  }
---
+++ src/pages/Resume.jsx
@@ -0,0 +1,10 @@
+export default function Resume() {
+    return (
+      <div>
+        <h1>My Resume</h1>
+        <div >
+            RESUME page is comming soon
+        </div>
+      </div>
+    );
+}
