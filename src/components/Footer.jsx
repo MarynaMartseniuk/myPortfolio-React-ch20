18569bfb3515907805f6be4b54959444400b9b0f
MarcM <marina.ved80gmail.com>
2024-10-06 22:36:33 -0600

    add content to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,33 @@
 import { Link, useLocation } from 'react-router-dom';
+import '../App.css';
+import './Footer.css';
 
 function NavTabs() {
   const currentPage = useLocation().pathname;
 
   return (
-    <div>
-        FOOTER is coming soon
+    <div class="container text-center mainContainer">
+      <div class="row">
+        <div id="footerContactMe" class="col-4">Contact Me:</div>
+        <div id="myData" class="col-8">
+        <ul>
+            <li id="phone">
+              phone: +1 (801) 349.68.36
+            </li>
+            <li id="email">
+              E-mail: marina.ved80@gmail.com
+            </li>       
+            <li id="LinkedIn">
+              <a class="myDataLink" href="https://www.linkedin.com/in/maryna-martseniuk/" 
+              target="_blank">LinkedIn</a>
+            </li>
+            <li id="GitHub" >
+              <a class="myDataLink" href="https://github.com/MarynaMartseniuk" 
+              target="_blank">GitHub</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
